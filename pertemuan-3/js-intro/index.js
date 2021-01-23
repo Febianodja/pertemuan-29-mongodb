1e766a4bfb624eb3fa97cbbde2e42ffd51fa0af7
febianodja <febianodja@gmail.com>
2021-01-23 22:50:45 +0700

    upload tugas

---
+++ pertemuan-3/js-intro/index.js
@@ -0,0 +1,52 @@
+// No. 1
+console.log("Nama: Febian Odja");
+console.log("Asal Daerah: Jakarta");
+console.log("Tanggal Lahir: 20 Februari 1992");
+console.log("Umur: 28 Tahun");
+console.log("Posisi Pekerjaan: Wiraswasta");
+
+// No. 2
+prompt("Nama User");
+prompt("Asal Negara");
+confirm("Apakah data yang diisi sudah benar?");
+alert("Terima kasih sudah mengisi form");
+
+// No.3 Single comments:
+// console.log("Nama: Febian Odja");
+// console.log("Asal Daerah: Jakarta");
+// console.log("Tanggal Lahir: 20 Februari 1992");
+// console.log("Umur: 28 Tahun");
+// console.log("Posisi Pekerjaan: Wiraswasta");
+
+// No.4
+/*
+prompt("Nama User");
+prompt("Asal Negara");
+confirm("Apakah data yang diisi sudah benar?");
+alert("Terima kasih sudah mengisi form");
+*/
+
+// No.5
+let dataDiri = {
+    nama: "Febian Odja",
+    asalDaerah: "Jakarta",
+    tanggalLahir: "20 Februari 1992",
+    umur: "28 Tahun",
+    posisiPekerjaan: "Wiraswasta"
+}
+console.log("Nama: " + dataDiri.nama);
+console.log("Asal daerah: " + dataDiri.asalDaerah);
+console.log("Tanggal Lahir: " + dataDiri.tanggalLahir);
+console.log("Umur: " + dataDiri.umur);
+console.log("Posisi Pekerjaan: " + dataDiri.posisiPekerjaan);
+
+// No.6
+let pertambahan = 25 + 10;
+let perkalian = 100 * 2;
+let pembagian = 99 / 2;
+let modulus = 99 % 2;
+
+console.log(pertambahan);
+console.log(perkalian);
+console.log(pembagian);
+console.log(modulus);
